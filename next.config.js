5f975a8e43ddf3506c10ed7aaa140be2d1974c8f
Jay.tr <jay@netlambda.com>
2022-03-09 00:19:17 +0700

    update deps

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   experimental: {
-      // concurrentFeatures: true,
-    //   serverComponents: true
-    reactRoot: false,
+    concurrentFeatures: true,
+    // serverComponents: true
+    // reactRoot: false,
   },
   reactStrictMode: false,
 };
