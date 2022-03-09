4a1a7011265556bfba07b1973592422c4cca1c68
Jay.tr <jay@netlambda.com>
2022-03-09 15:57:14 +0700

    fix build

--- src/components/PocoPrice.tsx
+++ src/components/PocoPrice.tsx
@@ -5,7 +5,7 @@ import { usePocoPriceQuery } from "../generated/graphql";
 
 function PocoPrice() {
   const [queryResult] = usePocoPriceQuery();
-  const price = new BigNumber(queryResult.data?.poco_price?.[0].price || 0)
+  const price = new BigNumber(queryResult.data?.token_price?.[0].price || 0)
     .div(1e18)
     .toFixed(4);
 
